d23e1896a2a79532f18c2d2383d0effa98bdc3a5
jdksec <44243011+jdksec@users.noreply.github.com>
2023-02-02 16:33:51 +0000

    Create logger.py

---
+++ custom/logger.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, render_template
+import os
+import html
+from datetime import datetime
+
+
+def sanitize_input(input_str):
+    return html.escape(input_str)
+
+
+def get_timestamp():
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return timestamp
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text_input = request.form.get("text_input")
+        clean = sanitize_input(text_input)
+        with open("log.txt", "a") as f:
+            f.write("[+] " + get_timestamp() + "\n")
+            f.write(clean + "\n")
+        return '''
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                <title>Ask a question</title>
+            </head>
+            <body>
+                <div class="container">
+                    <h1 class="text-center mt-5">Ask a question</h1>
+                    <form method="post">
+                        <div class="form-group">
+                            <textarea class="form-control" name="text_input"></textarea>
+                        </div>
+                        <br></br>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                    </form>
+                    <h1 class="text-centre mt-5">Answer</h1>
+                    <code class="text-left">
+                    {}
+                    </code>
+                </div>
+            </body>
+        </html>
+        '''.format(clean)
+    return '''
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            <title>Ask a question</title>
+        </head>
+        <body>
+            <div class="container">
+                <h1 class="text-center mt-5">Ask a question</h1>
+                <form method="post">
+                    <div class="form-group">
+                        <textarea class="form-control" name="text_input"></textarea>
+                    </div>
+                    <br></br>
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                </form>
+            </div>
+        </body>
+    </html>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=False)
