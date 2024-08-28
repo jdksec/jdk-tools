34ae2c51ec7e553a4ecdd53d70eba1270f1d4af5
jdksec <44243011+jdksec@users.noreply.github.com>
2024-08-28 12:41:00 +0100

    Update winaudit.bat

--- encoded/winaudit.bat
+++ encoded/winaudit.bat
@@ -251,19 +251,3 @@ schtasks /query /v /fo CSV > schtaskslist.csv
 reg query "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" /v EnableMulticast > llmnr.txt
 
 
-ECHO Saving SAM, System and Security...
-reg save HKLM\SAM SAM 2>&1
-reg save HKLM\System System 2>&1
-reg save HKLM\Security Security 2>&1
-
-
-ECHO Done!
-GOTO END
-
-:ERROR
-echo Permission error. Ensure you are an admin and have permissions to create folders. 
-echo.
-GOTO END
-
-:END
-ECHO.
